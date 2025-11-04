abe1581241c4eb2f13ab0d871a158d801664492e
sabari2309 <sabareeswarangopal@gmail.com>
2025-11-04 12:16:45 +0530

    loop increased

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Main Branch");
         System.err.println("This is from Modify branch");
-        for(int i=0;i<100;i++){
+        for(int i=0;i<5;i++){
             System.out.println("GIT");
         }
     }
